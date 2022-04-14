e121ea16654d7113fd0a6a681da884aa34c0f13f
Vasily <vasily-bazhenov.03@mail.ru>
2022-04-14 11:36:59 +0300

    feat: finish lesson15 hard

--- script.js
+++ script.js
@@ -1,45 +1,65 @@
+window.addEventListener('DOMContentLoaded', (event) => {
 'use strict'
 const body = document.querySelector('body')
 
-const DomElement = function (selector, height, width, bg, fontSize, text) {
+const DomElement = function (selector, height, width, bg, fontSize, position, text) {
     this.selector = selector
     this.height = height
     this.width = width
     this.bg = bg
     this.fontSize = fontSize
     this.text = text
+    this.position = position
 
-    const divCreate = () => {
-        if (selector != '.') {
-            const div = document.createElement('div')
+    this.divCreate = function () {
+        const div = document.createElement('div')
 
-            div.classList.add(selector)
+        if (this.selector.slice('')[0] == '.') {
+            div.classList.add(this.selector.slice(1))
 
-            div.style.height = height + 'px'
-            div.style.hwidth = width + 'px'
-            div.style.background = bg
-            div.style.fontSize = fontSize + 'px'
+            div.style.height = this.height + 'px'
+            div.style.width = this.width + 'px'
+            div.style.background = this.bg
+            div.style.fontSize = this.fontSize + 'px'
+            div.style.position = this.position
 
-            div.textContent = text
+            div.textContent = this.text
 
             body.append(div)
-        } else if (selector.charAt(0) = '#') {
-            const div = document.createElement('div')
+        } else if (this.selector.slice('')[0] == '#') {
+            div.id = this.selector.slice(1)
 
-            div.id = selector
+            div.style.height = this.height + 'px'
+            div.style.width = this.width + 'px'
+            div.style.background = this.bg
+            div.style.fontSize = this.fontSize + 'px'
+            div.style.position = this.position
 
-            div.style.height = height + 'px'
-            div.style.hwidth = width + 'px'
-            div.style.background = bg
-            div.style.fontSize = fontSize + 'px'
-
-            div.textContent = text
+            div.textContent = this.text
 
             body.append(div)
         }
     }
-    divCreate()
 }
 
-const divOne = new DomElement('.block', '300', '600', 'red', '16', 'Поясни за шмот')
+const divOne = new DomElement('.block', '100', '100', 'blue', '28', 'absolute', 'Поясни за шмот')
 
+
+divOne.divCreate()
+
+console.dir(window)
+
+document.addEventListener('keydown', e => {
+    const div = document.querySelector('.block')
+    console.dir(div);
+    if (e.code == 'ArrowRight' && div.offsetLeft < window.innerWidth - divOne.width) {
+        div.style.left = div.offsetLeft + 10 + 'px'
+    } else if (e.code == 'ArrowLeft' && div.offsetLeft > 0) {
+        div.style.left = div.offsetLeft - 10 + 'px'
+    } else if (e.code == 'ArrowDown' && div.offsetTop < window.innerHeight - divOne.height + 10) {
+        div.style.top = div.offsetTop + 10 + 'px'
+    } else if (e.code == 'ArrowUp' && div.offsetTop > 0) {
+        div.style.top = div.offsetTop - 10 + 'px'
+    }
+})
+});
